4e417e6804c122fd338a3d4a69b5525e56045f66
Liubov K <lyouso@yandex.ru>
2022-04-15 15:22:27 +0300

    Init commit

---
+++ Task 50/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение 
+этого элемента или же указание, что такого элемента нет. */
+
+int rows = 3;
+int columns = 4;
+int[,] array = new int[rows, columns];
+FillArray();
+PrintArray();
+Console.WriteLine();
+FindValue();
+
+void FillArray()
+{
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = rand.Next(0, 10);
+        }
+    }
+}
+void PrintArray()
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void FindValue()
+{
+    Console.WriteLine("Введите номер строки:");
+    int currentRow = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите номер колонки");
+    int currentColumn = Convert.ToInt32(Console.ReadLine());
+    if (currentRow >= rows || currentColumn >= columns)
+    {
+        Console.WriteLine("Такого элемента нет в массиве");
+    }
+    else Console.WriteLine(array[currentRow, currentColumn]);
+}
---
+++ Task 52/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+int rows = 3;
+int columns = 4;
+int[,] array = new int[rows, columns];
+int[] average = new int[4];
+FillArray();
+PrintArray();
+Console.WriteLine();
+FindAverage();
+
+
+
+void FillArray()
+{
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = rand.Next(0, 10);
+        }
+    }
+}
+void PrintArray()
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void FindAverage()
+{
+    for (int j = 0; j < columns; j++)
+    {
+        int average = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            average += array[i, j];
+        }
+        Console.Write(average/ rows + " ");
+    }
+    Console.WriteLine();
+}
